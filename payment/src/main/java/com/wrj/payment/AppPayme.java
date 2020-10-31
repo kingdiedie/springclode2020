7b1a64a7017cba4c9778d7b1234b496c002f19a8
king <523940687@qq.com>
2020-10-31 21:40:32 +0800

    项目初始化

---
+++ payment/src/main/java/com/wrj/payment/AppPayme.java
@@ -0,0 +1,11 @@
+package com.wrj.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPayme {
+    public static void main(String[] args) {
+        SpringApplication.run(AppPayme.class,args);
+    }
+}
